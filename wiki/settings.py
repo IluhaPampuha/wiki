d64b7d9735c04adfe5873a3b78094798bed7fd08
IluhaPampuha <cs-heroes@mail.ru>
2021-08-27 16:25:00 +0300

    2021-08-27

--- wiki/settings.py
+++ wiki/settings.py
@@ -39,7 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
 ]
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
